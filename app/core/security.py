7ba8a35842007527e4e09a4cd56ac7564e4cf8cf
Matthew Campbell <m.campbell92@gmail.com>
2026-03-24 20:46:09 +0100

    feat: begin work on security, json logic

---
+++ app/core/security.py
@@ -0,0 +1,36 @@
+import jwt
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+from jwt.exceptions import InvalidTokenError
+from pwdlib import PasswordHash
+from app.core.config import settings
+
+# use the helper class provided by the pwdlib package
+# the default recommended hasher is argon2
+password_hash = PasswordHash.recommended()
+
+def hash_password(password: str) -> str:
+    return password_hash.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> str:
+    return password_hash.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str) -> Optional[str]:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            return None
+        return email
+    except InvalidTokenError:
+        return None
