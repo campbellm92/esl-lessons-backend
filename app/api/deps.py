dfdaaa5ff6bf9cff78e514fc6a0660bdfa6c8c72
Matthew Campbell <m.campbell92@gmail.com>
2026-03-26 08:04:25 +0100

    feat: add oauth verification layer

---
+++ app/api/deps.py
@@ -0,0 +1,46 @@
+from fastapi import Security, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from app.db.database import get_db
+from app.core.security import verify_token
+from app.services.auth_service import AuthService
+from app.models.user import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_current_user(
+        credentials: HTTPAuthorizationCredentials = Security(oauth2_scheme), 
+        db: Session = Security(get_db)
+        ) -> User:
+    
+    credentials_exception = HTTPException(
+        status_code = status.HTTP_401_UNAUTHORIZED,
+        detail="Unable to validate credentials.",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+
+    email = verify_token(credentials.credential)
+    if email is None:
+        raise credentials_exception
+
+    user = AuthService.get_user_by_email(db, email)
+    if user is None:
+        raise credentials_exception
+
+    if not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Inactive user."
+        )
+
+    return user
+
+
+async def get_current_active_user(current_user: User = Security(get_current_user)) -> User:
+    if not current_user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Inactive user."
+        )
+    
+    return current_user
