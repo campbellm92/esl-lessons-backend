1520525ea2baa8e8e713edbb0d7d6ad10fa2e692
Matthew Campbell <m.campbell92@gmail.com>
2026-03-26 10:17:55 +0100

    refactor: bring in necessary imports, set up middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from app.core.config import settings
+from app.api import auth
+from app.db.database import engine
+from app.models import user
 
-app = FastAPI()
+user.Base.metadata.create_all(bind=engine)
 
+app = FastAPI(
+    title=settings.PROJECT_NAME
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
+
+app.include_router(auth.router, prefix=f"/auth", tags=["authentication"])
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def health_check():
+    return {
+        "message": f"{settings.PROJECT_NAME} is running.",
+        "status": "healthy"
+    }
