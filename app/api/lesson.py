522eae5b0b105a40415c02577e06b480e3bb16e5
Matthew Campbell <m.campbell92@gmail.com>
2026-04-03 15:34:30 +0200

    feat: add endpoint for getting lessons

---
+++ app/api/lesson.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.api.deps import get_db
+from app.models.lesson import Lesson
+
+router = APIRouter()
+
+# get a lesson by its slug
+@router.get("/{slug}")
+def get_lesson(slug: str, db: Session = Depends(get_db)):
+    return db.query(Lesson).filter(Lesson.slug == slug).first()
