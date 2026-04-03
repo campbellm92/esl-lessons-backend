7718e16789fcbe2eadbf92aa0657c8233e1d781d
Matthew Campbell <m.campbell92@gmail.com>
2026-04-03 12:32:54 +0200

    refactor: update lesson model to include slug and level

--- app/models/lesson.py
+++ app/models/lesson.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String, Integer, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.dialects.postgresql import JSONB, ARRAY
 from app.db.database import Base
 
 class Lesson(Base):
@@ -9,13 +9,13 @@ class Lesson(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, unique=True, index=True)
+    slug = Column(String, unique=True, index=True)
+    level = Column(ARRAY(String))
     content = Column(JSONB)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     
     ''' 
-    Relationship with lessons to be added later.
-    Example relationship:
-    tasks = relationship("Task", back_populates="owner", cascade="all, delete-orphan")
+    Relationship with other tables to be added later
     '''
 
