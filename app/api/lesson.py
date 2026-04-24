d5a55636e1a3b612ef341580f0984497f7c7985f
Matthew Campbell <m.campbell92@gmail.com>
2026-04-24 15:40:00 +0200

    feat: add route for all lessons

--- app/api/lesson.py
+++ app/api/lesson.py
@@ -5,6 +5,11 @@ from app.models.lesson import Lesson
 
 router = APIRouter()
 
+# get all lessons
+@router.get("/")
+def get_lessons(db: Session = Depends(get_db)):
+    return db.query(Lesson).all()
+
 # get a lesson by its slug
 @router.get("/{slug}")
 def get_lesson(slug: str, db: Session = Depends(get_db)):
